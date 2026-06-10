a856b1954c431def328bcd638e0ff7316b5f57e2
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-06-10 20:54:26 -0300

    fix: fixed crash on stringify

--- api/start.ts
+++ api/start.ts
@@ -1,12 +1,10 @@
 export default {
     async fetch(request: Request) {
-        return Response.json({ time: process.hrtime.bigint() / 1000n; },
-            {
-                headers:
-                {
-                    'content-type': 'application/json',
-                    'x-datacenter': request.headers.get('x-vercel-id')
-                }
-            });
+        return Response.json({ time: Number(process.hrtime.bigint() / 1000n) }, {
+            headers: {
+                'content-type': 'application/json',
+                'x-datacenter': request.headers.get('x-vercel-id')
+            }
+        });
     },
 };
