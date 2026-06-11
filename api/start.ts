ed3cc89f4482ddc516ddbac4657c50dc527272ca
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-06-10 21:01:22 -0300

    fix: vercel bun does not accept micro

--- api/start.ts
+++ api/start.ts
@@ -1,6 +1,6 @@
 export default {
     async fetch(request: Request) {
-        return Response.json({ time: Number(process.hrtime.bigint() / 1000n) }, {
+        return Response.json({ time: performance.now() * 1000 }, {
             headers: {
                 'content-type': 'application/json',
                 'x-datacenter': request.headers.get('x-vercel-id')
