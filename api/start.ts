f59e6736b935c7a6c9dcad6ea63b045e0fd579dc
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-06-10 21:36:41 -0300

    fix: replaced perf with date

--- api/start.ts
+++ api/start.ts
@@ -1,9 +1,8 @@
 export default {
     async fetch(request: Request) {
-        return Response.json({ time: performance.now() * 1000 }, {
+        return Response.json({ time: Date.now() * 1000 }, {
             headers: {
                 'content-type': 'application/json',
-                'x-datacenter': request.headers.get('x-vercel-id')
             }
         });
     },
